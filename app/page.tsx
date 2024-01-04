b885bcecc54fcd054e227bdb01b1a79d7e69fb0c
joaoleandro <new.joaoleandro@gmail.com>
2024-01-04 19:59:39 -0300

    segundo commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,17 @@ export default function Home() {
   return (
     <main>
         <Container>
-          <div className="p-3 mt-10">
+          <div className="p-3 mt-10 w-full flex gap-3">
               <Cards>
                 <div className="bg-green-300 h-full">
+                  <data>04/01/24</data>
+                </div>
+              </Cards>
 
+              <Cards>
+                <div className="h-full bg-red-300">
+                    <h2>Teste</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem similique natus dolor earum reprehenderit vero expedita rem animi alias sunt. Enim molestiae vel distinctio a maiores fugiat suscipit aperiam ab.</p>
                 </div>
               </Cards>
           </div>
--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -2,10 +2,10 @@ import Container from "../Container";
 
 const Footer = () => {
     return ( 
-        <footer className="bg-green-300">
+        <footer className="bg-sky-900">
             <Container>
-                <div className="w-full bg-red-300 flex justify-center items-center p-3">
-                    <h2>Conteudo do meu footer</h2>
+                <div className="w-full flex justify-center items-center p-3">
+                    <h2 className="font-bold">Conteudo do meu footer</h2>
                 </div>
             </Container>
         </footer>
